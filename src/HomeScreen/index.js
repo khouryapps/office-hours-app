d10e18702037b3a6e8398505ed9c6d0d179db683
Grisha <grish2580@gmail.com>
2019-06-11 12:48:53 -0400

    Basic navigation is working

---
+++ src/HomeScreen/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import HomeScreen from "./HomeScreen.js";
+import MainScreenNavigator from "../ChatScreen/index.js";
+import ProfileScreen from "../ProfileScreen/index.js";
+import SideBar from "../SideBar/SideBar.js";
+import { createDrawerNavigator } from "react-navigation";
+
+const HomeScreenRouter = createDrawerNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Chat: { screen: MainScreenNavigator },
+    ProfileScreen: { screen: ProfileScreen }
+  },
+  {
+    contentComponent: props => <SideBar {...props} />
+  }
+);
+
+export default HomeScreenRouter;
