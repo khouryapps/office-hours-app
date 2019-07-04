65e03e25fe1dc49316a351161571c99dd3566426
wstenzel <wills.stenzel@gmail.com>
2019-07-04 12:44:31 -0400

    Updated package.json and added setup file so tests will run

---
+++ __tests__/setup.js
@@ -0,0 +1,30 @@
+//setup.js
+jest.mock('Linking', () => {
+    return {
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        openURL: jest.fn(),
+        canOpenURL: jest.fn(),
+        getInitialURL: jest.fn(),
+    }
+})
+
+jest.mock('react-navigation', () => {
+    return {
+        createAppContainer: jest.fn().mockReturnValue(function NavigationContainer(props) {return null;}),
+        createDrawerNavigator: jest.fn(),
+        createMaterialTopTabNavigator: jest.fn(),
+        createStackNavigator: jest.fn(),
+        StackActions: {
+            push: jest.fn().mockImplementation(x => ({...x,  "type": "Navigation/PUSH"})),
+            replace: jest.fn().mockImplementation(x => ({...x,  "type": "Navigation/REPLACE"})),
+        },
+        NavigationActions: {
+            navigate: jest.fn().mockImplementation(x => x),
+        }
+    }
+});
+
+jest.mock('WebView', () => 'WebView');
+
+jest.mock('DatePickerIOS', () => 'DatePickerIOS');
