6f7bcbc746fabd119b572cadfcc9ee97f1c610e5
wstenzel <wills.stenzel@gmail.com>
2019-07-17 18:59:42 -0700

    Added button to finish helping with all tickets at once

--- src/TA/QueueScreen/CurrentlyHelping.js
+++ src/TA/QueueScreen/CurrentlyHelping.js
@@ -3,13 +3,23 @@ import {Button, Card, CardItem, View, H2, Text} from "native-base"
 import ShowTicket from "./ShowTicket";
 
 export default class CurrentlyHelping extends React.Component {
+
+    updateAllStatus = () => {
+        this.props.tickets.map(el => this.props.updateStatus(el.id, "Closed"))
+    }
     render() {
         const {tickets} = this.props;
         if (tickets.length) {
             return (
                 <View>
                     <H2>Currently Helping</H2>
-                    {tickets.map(el => <ShowTicket updateStatus={this.props.updateStatus} showButtonOnStatus={"In Progress"} key={el.id} {...el}/>)}
+                    {tickets.map(el => <ShowTicket updateStatus={this.props.updateStatus}
+                                                   showButtonOnStatus={"In Progress"} key={el.id} {...el}/>)}
+                    {tickets.length > 1 ?
+                        (<Button onPress={() => this.updateAllStatus()} style={{flex:1, justifyContent: 'center'}}>
+                            <Text>Finish Helping All</Text>
+                        </Button>) :
+                        null}
                 </View>)
         } else {
             return null
