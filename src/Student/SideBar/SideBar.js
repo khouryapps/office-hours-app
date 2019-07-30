c0355170a32f6ff9294ad12b176de2bdeb27fc43
wstenzel <wills.stenzel@gmail.com>
2019-07-29 17:58:45 -0700

    Added fetching component so the sidebar information is now connected with the data in the database for the students info

--- src/Student/SideBar/SideBar.js
+++ src/Student/SideBar/SideBar.js
@@ -16,12 +16,33 @@ const routes = ["", "Chat", "Profile"];
 export default class SideBar extends React.Component {
     state = {
         photo: "http://s3.amazonaws.com/37assets/svn/765-default-avatar.png",
-        student_name: 'Will Stenzel',
-        courses_list: ["CS3500", "CS5000", "CS1700"],
-        isTA: false
+        student_name: null,
+        courses_list: [],
+        isTA: false,
+        loading: true,
     };
 
+    fetch_courses_list = async () => {
+        try {
+            //Assign the promise unresolved first then get the data using the json method.
+            const studentAPICall = await fetch('http://127.0.0.1:8002/api/officehours/me/',
+                {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': "Token a891e91d45001088b201b3c2ebe8a5e87a9121f9"
+                    },
+                    });
+            const student_details = await studentAPICall.json()
+            this.setState({courses_list: student_details.courses, student_name: student_details.full_name, loading: false});
+        } catch(err) {
+            console.log("Error fetching data-----------", err);
+        }
+    }
+
     async componentDidMount(){
+        await this.fetch_courses_list();
         this.setState({isTA: await AsyncStorage.getItem('isTA')})
     }
 
